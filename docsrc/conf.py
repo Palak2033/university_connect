59b17c98739639cd6b9e211b1b764dd31d69781d
Palak Goel <f20180286@goa.bits-pilani.ac.in>
2021-11-27 23:30:21 +0530

    Updated docs

--- docsrc/conf.py
+++ docsrc/conf.py
@@ -36,6 +36,7 @@ release = '1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.todo', 'sphinx.ext.viewcode', 'sphinx.ext.autodoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -57,4 +58,4 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['../docs/_static']
